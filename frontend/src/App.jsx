3fe39c544d1c67aa35fc398036924ffca080a895
Sachin Rawat <119297666+sachin2002rawat@users.noreply.github.com>
2025-09-25 23:44:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import ViewLecture from './pages/ViewLecture'
 import SearchWithAi from './pages/SearchWithAi'
 import getAllReviews from './customHooks/getAllReviews'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = " https://lms-a79h.onrender.com"
 
 function App() {
   
